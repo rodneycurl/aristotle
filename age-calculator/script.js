472f5badfdcc06e8c89dad006aee2f134c1704af
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-05-31 09:54:52 -0400

    added comments

--- age-calculator/script.js
+++ age-calculator/script.js
@@ -1,4 +1,8 @@
+//pop-up to ask for users age
 var age = prompt("How old are you?");
+
+//equation for calculating age in days
 var days = age * 365;
 
+//output for the user to see how old they are in days
 alert(age + " years is roughly " + days + " days.");
