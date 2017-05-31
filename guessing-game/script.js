62589bcabc1d97456304e6947a2b40952a149dc4
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-05-31 12:28:59 -0400

    added right answer if statement

--- guessing-game/script.js
+++ guessing-game/script.js
@@ -1,1 +1,10 @@
-alert("it's working");
+//create secretNumber
+var secretNumber = 4
+
+//ask user for guess
+var guess = prompt("Guess a number.")
+
+//check if guess is correct
+if(Number(guess) === secretNumber) {
+  alert("YOU GOT IT RIGHT!");
+}
