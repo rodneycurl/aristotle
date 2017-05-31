8f2bf91f0bb21a4d4d741c521b81d4ef012ad382
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-05-30 20:25:40 -0400

    fixing semantics

--- dce-script.js
+++ dce-script.js
@@ -1,5 +1,6 @@
 var firstName = prompt("What's your first name?");
 var lastName = prompt("What's your last name?");
 var userAge = prompt("How old are you?");
+
 console.log ("Your full name is " + firstName + " " + lastName + ".");
-console.log("You are " +  userAge + " years old.");
+console.log("You are " + userAge + " years old.");
