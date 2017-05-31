aefba78a7bbc8d1e14989267f323a2f84559dfb3
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-05-31 13:21:13 -0400

    making sure files are updated

--- guessing-game/script.js
+++ guessing-game/script.js
@@ -2,9 +2,20 @@
 var secretNumber = 4
 
 //ask user for guess
-var guess = prompt("Guess a number.")
+var stringGuess = prompt("Guess a number.");
+var guess = Number(stringGuess);
 
 //check if guess is correct
-if(Number(guess) === secretNumber) {
+if(guess === secretNumber) {
   alert("YOU GOT IT RIGHT!");
+}
+
+//otherwise, check if guess is higher
+else if(guess > secretNumber){
+  alert("Too high. Guess again!");
+}
+
+//otherwise, check if guess is lower
+else {
+  alert("Too low. Guess again!");
 }
