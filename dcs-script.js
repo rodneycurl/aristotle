255dd16ce86c27e8ba4405032a8b247840fe4189
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-05-30 20:08:26 -0400

    setting variables for users first name, last name, age, and printing out their full name and age in a sentence

--- dcs-script.js
+++ dcs-script.js
@@ -1,2 +1,5 @@
-prompt("What's your first name");
-prompt("What's your last name");
+var firstName = prompt("What's your first name?");
+var lastName = prompt("What's your last name?");
+var userAge = prompt("How old are you?");
+console.log ("Your full name is " + "firstName " + "lastName.")
+console.log("You are " + "userAge years old.")
