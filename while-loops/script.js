5c6c20abccb21a1c0b1cd5f9d3ee9537bc3e9ccb
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-05-31 19:53:23 -0400

    adding js for printing all even numbers between 10 and 40

--- while-loops/script.js
+++ while-loops/script.js
@@ -1,1 +1,21 @@
-alert("YES!")
+// Print all numbers between -10 and 19
+console.log("PRINT ALL NUMBERS BETWEEN -10 AND 19");
+var counter = -10;
+
+while(counter < 20) {
+  console.log(counter);
+  counter++;
+}
+
+// Print all even numbers between 10 and 40
+console.log("PRINT ALL EVEN NUMBERS BETWEEN 10 AND 40");
+var counter = 10;
+
+while(counter <= 40) {
+  console.log(counter);
+  counter+=2;
+}
+
+// Print all odd numbers between 300 and 333
+
+// Print all numbers divisible by 5 AND 3 between 5 and 50
