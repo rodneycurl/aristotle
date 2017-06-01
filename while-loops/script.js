113003767fea33c75a506fb8ea4eed3e339d1440
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-05-31 20:00:45 -0400

    added js to print odd numbers between 300 and 333

--- while-loops/script.js
+++ while-loops/script.js
@@ -17,5 +17,14 @@ while(counter <= 40) {
 }
 
 // Print all odd numbers between 300 and 333
+console.log("PRINT ALL ODD NUMBERS BETWEEN 300 AND 333");
+var counter = 300;
+
+while(counter <= 333) {
+  if(counter % 2 !== 0) {
+  console.log(counter);
+  }
+  counter+=1;
+}
 
 // Print all numbers divisible by 5 AND 3 between 5 and 50
