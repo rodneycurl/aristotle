a7fbb7cef38ce7a51ddcce131280f7d4cb899522
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-05 19:25:38 -0400

    first attempt

---
+++ for-loops/script.js
@@ -0,0 +1,28 @@
+// Print all numbers between -10 and 19
+console.log("PRINT ALL NUMBERS BETWEEN -10 AND 19")
+
+for(var i = -10; i < 20; i++) {
+  console.log(i)
+}
+
+// Print all even numbers between 10 and 40
+console.log("PRINT ALL EVEN NUMBERS BETWEEN 10 AND 40")
+
+for(var i = 10; i <= 40; i+=2) {
+  console.log(i)
+}
+
+// Print all odd numbers between 300 and 333
+console.log("PRINT ALL ODD NUMBERS BETWEEN 300 AND 333")
+
+for(var i = 300; i <= 333; i++) {
+  if(i % 2 !== 0)
+  console.log(i)
+}
+
+// Print all numbers divisible by 5 AND 3 between 5 and 50
+console.log("PRINT ALL NUMBERS DIVISIBLE BY 5 AND 3 between 5 and 50")
+
+for(var i = 5; i <= 50; i++) {
+  console.log(i)
+}
