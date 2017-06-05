46d695571b2a9cea33b3b2c341d065f7f8434394
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-05 19:31:46 -0400

    ;

--- for-loops/script.js
+++ for-loops/script.js
@@ -2,14 +2,14 @@
 console.log("PRINT ALL NUMBERS BETWEEN -10 AND 19")
 
 for(var i = -10; i < 20; i++) {
-  console.log(i)
+  console.log(i);
 }
 
 // Print all even numbers between 10 and 40
 console.log("PRINT ALL EVEN NUMBERS BETWEEN 10 AND 40")
 
 for(var i = 10; i <= 40; i+=2) {
-  console.log(i)
+  console.log(i);
 }
 
 // Print all odd numbers between 300 and 333
