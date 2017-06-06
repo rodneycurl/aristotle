fe64810bcb2bbdb4a602231af3c7921ed81ee917
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-05 20:31:56 -0400

    creating an isEven function

---
+++ functions/script.js
@@ -0,0 +1,12 @@
+//write a function isEven() which takes a single numeric argument and returns true if the number is even, and false otherwise
+
+function isEven(num){
+  //return true if even
+  if(num % 2 === 0) {
+    return true;
+  }
+  //return false otherwise
+  else {
+    return false
+  }
+}
