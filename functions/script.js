1c95404db1086dd144b0f2218a552da43a408a49
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-05 20:37:57 -0400

    adding factorial problem to html

--- functions/script.js
+++ functions/script.js
@@ -1,18 +1,21 @@
 //write a function isEven() which takes a single numeric argument and returns true if the number is even, and false otherwise
 
-// function isEven(num){
-//   //return true if even
-//   if(num % 2 === 0) {
-//     return true;
-//   }
-//   //return false otherwise
-//   else {
-//     return false
-//   }
-// }
+// 1. isEven FUNCTION
+function isEven(num){
+  //return true if even
+  if(num % 2 === 0) {
+    return true;
+  }
+  //return false otherwise
+  else {
+    return false
+  }
+}
 
 //refactored version
 
 function isEven(num){
   if num % 2 === 0;
 }
+
+// 2. factorial() FUNCTION
