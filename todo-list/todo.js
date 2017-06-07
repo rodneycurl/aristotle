45cd7fe2141fd0b1d91b2fd07d56820126720b14
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-07 16:35:31 -0400

    setup todos list in array, set prompt for user input, and added a way for the user to add new task

--- todo-list/todo.js
+++ todo-list/todo.js
@@ -1,1 +1,10 @@
-console.log("Connected!");
+//use an array to model todo list
+var todos = ["Buy New Turtle"];
+
+//ask user for input
+var input = prompt("What would you like to do?");
+
+//check what input is
+if(input === "list") {
+  console.log(todos);
+}
