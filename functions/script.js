c293c7cd8f28f5919255b06d2ba918c5e4c5b483
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-07 15:06:42 -0400

    sudo code

--- functions/script.js
+++ functions/script.js
@@ -1,1 +1,8 @@
 //write a function isEven() which takes a single numeric argument and returns true if the number is even, and false otherwise
+
+//isEven
+
+function isEven(num){
+  //return true if even
+  //return false otherwise
+}
