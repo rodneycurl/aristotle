c767f3c3b057f79c25ce269729a95f0cd636f97e
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-07 15:11:32 -0400

    refactored the first isEven approach

--- functions/script.js
+++ functions/script.js
@@ -1,14 +1,18 @@
 //write a function isEven() which takes a single numeric argument and returns true if the number is even, and false otherwise
 
-//isEven
+//2. isEven
 
+// function isEven(num){
+//   //return true if even
+//   if(num % 2 === 0) {
+//     return true;
+//   }
+//   //return false otherwise
+//   else {
+//     return false
+//   }
+
+//2. isEven refactored
 function isEven(num){
-  //return true if even
-  if(num % 2 === 0) {
-    return true;
+    return num % 2 === 0;
   }
-  //return false otherwise
-  else {
-    return false
-  }
-}
