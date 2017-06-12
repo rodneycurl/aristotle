561d6e357534150f53f4486a08f4fdd1d149a6bb
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-12 12:14:17 -0400

    adding moviedb

--- intro-to-js-objects/script.js
+++ intro-to-js-objects/script.js
@@ -20,3 +20,29 @@ var post = [
     comments: ["<3", "Whattttt????"]
   }
 ]
+
+//example of array of post (nested) using movieDB
+
+var movie = [
+  {
+    title: "Friday",
+    rating: "5 stars",
+    hasWatched: "You have watched "
+  },
+  {
+    title: "Next Friday",
+    rating: "4.5 stars",
+    hasWatched: "You have watched "
+  },
+  {
+    title: "Friday After Next",
+    rating: "3.5 stars",
+    hasWatched: "You have watched "
+  },
+  {
+    title: "Are We There Yet 5",
+    rating: "1.2 stars",
+    hasWatched: "You have not seen "
+  },
+
+]
