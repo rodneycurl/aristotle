1f1f3ae32919db22d557fd97fabb5477402afa1a
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-12 12:02:25 -0400

    building example for object formatting and also an array with posts nested

--- intro-to-js-objects/script.js
+++ intro-to-js-objects/script.js
@@ -1,7 +1,21 @@
-//objects format
+//objects format example
 
-var name = {
-  item: "x",
-  item: "x",
-  item: "x"
+var car = {
+  year: "2017",
+  make: "Range Rover",
+  color: "Silver"
 };
+
+//example of array of posts (nested)
+
+var post = [
+  {
+    title: "NBA Finals",
+    author: "Stephen A. Smith"
+    comments: []
+  },
+  {
+    title: "The Cavs In 7"
+    author: "Skip A. Bayless"
+  }
+]
