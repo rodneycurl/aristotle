b7dc6240954198bfd9ef790a519956f3d3eba42b
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-12 12:33:10 -0400

    added an example of a js method

--- js-methods/script.js
+++ js-methods/script.js
@@ -1,3 +1,15 @@
 console.log("It's working!")
 
 //a method is just a function that is a property inside of an object - colt steele
+
+//js method example below:
+
+var obj = {
+  name: "Chuck",
+  age: 45,
+  isCool: false,
+  friends: ["bob","tina"],
+  add: function(x,y) {
+    return x + y;
+  }
+}
