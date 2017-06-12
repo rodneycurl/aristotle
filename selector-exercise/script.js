7d2931b3cc62dd458daa074a5dcfa99e047ab751
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-12 16:39:49 -0400

    showing different ways to select the first paragraph

--- selector-exercise/script.js
+++ selector-exercise/script.js
@@ -1,1 +1,16 @@
-console.log("It's aliveeee!")
+
+// How to select the first paragraph in various different ways in the console
+
+document.getElementById("first");
+
+document.querySelector("#first");
+
+document.getElementByClassName("special")[0]
+
+document.querySelector(".special");
+
+document.getElementsByTagName("p")[0]
+
+document.querySelector("p");
+
+document.querySelectorAll("p")[0]
