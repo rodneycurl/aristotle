afb37a9c13b67c775a2a98ac2ed3c68745e94009
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-12 12:21:58 -0400

    added a for loop to print out the results

--- intro-to-js-objects/script.js
+++ intro-to-js-objects/script.js
@@ -43,6 +43,16 @@ var movie = [
     title: "Are We There Yet 5",
     rating: "1.2 stars",
     hasWatched: "You have not seen "
-  },
-
+  }
 ]
+movie.forEach(function(movie){
+  var result = "You have "
+  if(movie.hasWatched){
+    result += "watched ";
+  } else {
+    result += "not seen ";
+  }
+  result += "\"" + movie.title + "\" - ";
+  result += movie.rating + " ";
+  console.log(result)
+})
