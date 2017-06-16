789d9ca80c2f16b8845733c2340315de316c5696
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-16 19:08:23 -0400

    reset - will now make both scores go back to zero

--- score-keeper/script.js
+++ score-keeper/script.js
@@ -1,5 +1,6 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var p1Score = 0;
@@ -27,4 +28,14 @@ p2Button.addEventListener("click", function (){
     }
     p2Display.textContent = p2Score;
   }
+});
+
+resetButton.addEventListener("click", function(){
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
+  gameOver = false;
 });
