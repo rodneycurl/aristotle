7a1138668e7cedb74663b235dd2c410256cf017f
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-16 18:33:24 -0400

    creating an alert for when a player one button is clicked

--- score-keeper/script.js
+++ score-keeper/script.js
@@ -1,1 +1,6 @@
-alert("Connected!");
+var p1Button = document.querySelector("#p1");
+var p2Button = document.getElementById"p2");
+
+p1Button.addEventListener("click", function(){
+  alert("CLICKED!");
+});
