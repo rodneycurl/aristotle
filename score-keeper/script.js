2720a53fe53df5f2eb5a52775564455d48c9b574
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-16 18:34:00 -0400

    fixed a bug

--- score-keeper/script.js
+++ score-keeper/script.js
@@ -1,5 +1,5 @@
 var p1Button = document.querySelector("#p1");
-var p2Button = document.getElementById"p2");
+var p2Button = document.getElementById("p2");
 
 p1Button.addEventListener("click", function(){
   alert("CLICKED!");
