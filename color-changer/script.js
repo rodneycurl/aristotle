b865ecdbecc7d97a945cb38ca48254360637a8cd
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-16 18:25:37 -0400

    create a toggle that coop with a style

--- color-changer/script.js
+++ color-changer/script.js
@@ -1,12 +1,15 @@
 var button = document.querySelector("button");
 var isPurple = false;
 
+// button.addEventListener("click", function (){
+//   if(isPurple){
+//     document.body.style.background = "white";
+//   } else {
+//     document.body.style.background = "purple";
+//   }
+//   isPurple = !isPurple;
+// });
+
 button.addEventListener("click", function (){
-  if(isPurple){
-    document.body.style.background = "white";
-    isPurple = false;
-  } else {
-    document.body.style.background = "purple";
-    isPurple = true;
-  }
+  document.body.classList.toggle("purple");
 });
