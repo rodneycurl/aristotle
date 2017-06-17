2cb5776801c3521ae4a6f73047c67eb9ba90d348
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-17 08:42:11 -0400

    refactoring code to apply hover effect to all lis via for loop using mouseover and mouseout

--- todo-plus/script.js
+++ todo-plus/script.js
@@ -1,9 +1,10 @@
-var firstLI = document.querySelector("li");
+var lis = document.querySelectorAll("li");
 
-firstLI.addEventListener("mouseover", function(){
-  firstLI.style.color = "green";
-});
-
-firstLI.addEventListener("mouseout", function(){
-  firstLI.style.color = "black";
-});
+for(var i = 0; i < lis.length; i++){
+  lis[i].addEventListener("mouseover", function(){
+    this.style.color = "green";
+  });
+  lis[i].addEventListener("mouseout", function(){
+    this.style.color = "black";
+  });
+}
