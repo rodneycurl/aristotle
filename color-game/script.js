24721db7687e8be1866619b8876a5ff355a3b3b2
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-17 09:19:33 -0400

    fixed background bug

--- color-game/script.js
+++ color-game/script.js
@@ -10,5 +10,5 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 
 for(var i = 0; i < squares.length; i++){
-  squares[i].style.backgroundColor = colors[i]
+  squares[i].style.backgroundColor = colors[i];
 }
