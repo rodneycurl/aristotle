7c2929881a9708414589ddf8769eff9ff8aaf98b
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-17 17:16:15 -0400

    creating function to generate random colors

--- color-game/script.js
+++ color-game/script.js
@@ -1,11 +1,4 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -44,4 +37,19 @@ function changeColors(color){
 function pickColor(){
   var random = Math.floor(Math.random() * colors.length)
   return colors[random];
+}
+
+function generateRandomColors(num){
+  //make an array
+  var arr = []
+  //repeat num times
+  for(var i = 0; i < num; i++){
+    //get random color and push into arr
+  }
+  //return array
+  return arr;
+}
+
+function randomColor(){
+  
 }
