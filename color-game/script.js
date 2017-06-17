6711ee905bf8a805db5b7b8a38ee833b897d4163
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-17 17:48:12 -0400

    added reset to h1 background color once the reset button is clicked

--- color-game/script.js
+++ color-game/script.js
@@ -4,6 +4,21 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+  //generate all new colors
+  colors = generateRandomColors(6);
+  //pick a new random color from array
+  pickedColor = pickColor();
+  //change colorDisplay to match picked color
+  colorDisplay.textContent = pickedColor;
+  //change colors of squares
+  for(var i = 0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = "#232323";
+})
 
 colorDisplay.textContent = pickedColor;
 
@@ -18,11 +33,12 @@ for(var i = 0; i < squares.length; i++){
     //compare color to pickColor
     console.log(clickedColor, pickedColor);
     if(clickedColor === pickedColor){
-      messageDisplay.textContent = "Correct";
+      messageDisplay.textContent = "Correct!";
+      resetButton.textContent = "Play Again?";
       changeColors(clickedColor);
-      h1.style.background = clickedColor;
+      h1.style.backgroundColor = clickedColor;
     } else {
-      this.style.background = "#232323";
+      this.style.backgroundColor = "#232323";
       messageDisplay.textContent = "Try Again";
 
     }
@@ -33,7 +49,7 @@ function changeColors(color){
   //loop through all squares
   for(var i = 0; i < colors.length; i++)
   //change each color to match given color
-  squares[i].style.background = color;
+  squares[i].style.backgroundColor = color;
 }
 
 function pickColor(){
