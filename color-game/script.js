fb322c4995916155133d52d0e4439f5e5a6fae33
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-19 10:39:28 -0400

    shortened the code

--- color-game/script.js
+++ color-game/script.js
@@ -40,51 +40,8 @@ function reset(){
   h1.style.backgroundColor = "#steelblue";
 }
 
-// easyBtn.addEventListener("click", function(){
-//   hardBtn.classList.remove("selected");
-//   easyBtn.classList.add("selected");
-//   numSquares = 3;
-//   colors = generateRandomColors(numSquares);
-//   pickedColor = pickColor();
-//   colorDisplay.textContent = pickedColor;
-//   for(var i =0; i < squares.length; i++){
-//     if(colors[i]){
-//       squares[i].style.background = colors[i];
-//     } else {
-//       squares[i].style.display = "none";
-//     }
-//   }
-// });
-//
-// hardBtn.addEventListener("click", function(){
-//   hardBtn.classList.add("selected");
-//   easyBtn.classList.remove("selected");
-//   numSquares = 6;
-//   colors = generateRandomColors(numSquares);
-//   pickedColor = pickColor();
-//   colorDisplay.textContent = pickedColor;
-//   for(var i =0; i < squares.length; i++){
-//       squares[i].style.background = colors[i];
-//       squares[i].style.display = "block";
-//     }
-// });
-
 resetButton.addEventListener("click", function(){
   reset();
-  // //generate all new colors
-  // colors = generateRandomColors(numSquares);
-  // //pick a new random color from array
-  // pickedColor = pickColor();
-  // //change colorDisplay to match picked color
-  // colorDisplay.textContent = pickedColor;
-  // this.textContent = "New Colors";
-  //
-  // messageDisplay.textContent = "";
-  // //change colors of squares
-  // for(var i = 0; i < squares.length; i++){
-  //   squares[i].style.backgroundColor = colors[i];
-  // }
-  // h1.style.backgroundColor = "#steelblue";
 })
 
 colorDisplay.textContent = pickedColor;
