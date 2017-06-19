8e162165bd975ab8e7c948dc96b92c6faeec814a
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-19 10:45:55 -0400

    drying the code

--- color-game/script.js
+++ color-game/script.js
@@ -1,22 +1,48 @@
 var numSquares = 6;
-var colors = generateRandomColors(numSquares);
+var colors = [];
+var pickedColor;
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
 var modeButtons = document.querySelectorAll(".mode");
 
-for(var i = 0; i < modeButtons.length; i++){
-  modeButtons[i].addEventListener("click", function(){
-    modeButtons[0].classList.remove("selected");
-    modeButtons[1].classList.remove("selected");
-    this.classList.add("selected");
-    //ternary operator is used on the code below think of it as an if else statement with two options
-    this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
-    reset();
-  });
+init();
+
+function init(){
+  //mode buttons event listeners
+  for(var i = 0; i < modeButtons.length; i++){
+    modeButtons[i].addEventListener("click", function(){
+      modeButtons[0].classList.remove("selected");
+      modeButtons[1].classList.remove("selected");
+      this.classList.add("selected");
+      //ternary operator is used on the code below think of it as an if else statement with two options
+      this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+      reset();
+    });
+  }
+
+  //square event listeners
+  for(var i = 0; i < squares.length; i++){
+    //add click listeners to squares
+    squares[i].addEventListener("click", function (){
+      //grab color of clicked squares
+      var clickedColor = this.style.backgroundColor;
+      //compare color to pickColor
+      if(clickedColor === pickedColor){
+        messageDisplay.textContent = "Correct!";
+        resetButton.textContent = "Play Again?";
+        changeColors(clickedColor);
+        h1.style.backgroundColor = clickedColor;
+      } else {
+        this.style.backgroundColor = "#232323";
+        messageDisplay.textContent = "Try Again";
+      }
+    });
+  }
+
+  reset();
 }
 
 function reset(){
@@ -44,18 +70,12 @@ resetButton.addEventListener("click", function(){
   reset();
 })
 
-colorDisplay.textContent = pickedColor;
-
 for(var i = 0; i < squares.length; i++){
-  //add initial colors to squares
-  squares[i].style.backgroundColor = colors[i];
-
   //add click listeners to squares
   squares[i].addEventListener("click", function (){
     //grab color of clicked squares
     var clickedColor = this.style.backgroundColor;
     //compare color to pickColor
-    console.log(clickedColor, pickedColor);
     if(clickedColor === pickedColor){
       messageDisplay.textContent = "Correct!";
       resetButton.textContent = "Play Again?";
@@ -64,7 +84,6 @@ for(var i = 0; i < squares.length; i++){
     } else {
       this.style.backgroundColor = "#232323";
       messageDisplay.textContent = "Try Again";
-
     }
   });
 }
