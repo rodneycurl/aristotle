3e3a7ce104708171802aaa3eb3cf57f1b3adece1
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-19 13:12:53 -0400

    utilizing jQuery selection via JavaScript to change styles

--- jquery-exercise/script.js
+++ jquery-exercise/script.js
@@ -1,1 +1,11 @@
+//select all divs and give them a purple background
 $("div").css("background", "purple");
+
+//select the divs with class "highlight" and make them 200px width
+$("div.highlight").css("width", "200px");
+
+//select the div with the id "third" and give it an orange border
+$("div#third").css("border", "4px solid orange");
+
+//bonus: select the first div only and change its font color to pink
+$("div:first-of-type").css("color", "pink");
