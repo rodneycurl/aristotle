bd2ba05394580f7bccbd2ddcce35366cffc32107
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-20 09:31:43 -0400

    removing line-through on second click and putting black into object

--- jquery-todo-app/assets/js/script.js
+++ jquery-todo-app/assets/js/script.js
@@ -3,14 +3,17 @@ $("li").click(function(){
   //if li is gray
   if($(this).css("color") === "rgb(128, 128, 128)"){
       //turn it black
-      $(this).css("color", "black");
+      $(this).css({
+        color: "black",
+        textDecoration: "none",
+      });
   }
   //else
   else {
+    //turn it gray
     $(this).css({
       color: "gray",
       textDecoration: "line-through",
     });
   }
-    //turn it gray
 });
