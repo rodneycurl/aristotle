030cc240d084d0b78a57e71c019aad05a8a7ffc7
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-26 11:23:12 -0400

    adding a remove method

--- cs-pilot/script.js
+++ cs-pilot/script.js
@@ -19,21 +19,44 @@ class List {
 
 
   growArray() {
-    //create a new array twice as big as the current array
+    // Create a new array twice as big as the current array
     var newArray = new Array(this.data.length * 2);
-    
-    //copy everything from the old array to the new array
+
+    // Copy everything from the old array to the new array
     for (var i = 0; i < this.data.length; i++) {
       newArray[i] = this.data[i];
     }
 
-    //replace the reference from the old array to the new array
+    // Replace the reference from the old array to the new array
     this.data = newArray;
   }
 
 
   get(index) {
     return this.data[index];
+  }
+
+  remove(index) {
+    // Just return false if the list is already empty.
+    if (this.size == 0) {
+      return false;
+    }
+
+    // Save the current data at the index so it can be returned at the end.
+    var result = this.data[index];
+
+
+    // Start the element that's supposed to be removed and shift
+    // everything over by one.
+
+    for (var i = index; i < this.size - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+
+    // Decrement the total size of the list.
+    this.size--;
+
+    return result;
   }
 }
 
