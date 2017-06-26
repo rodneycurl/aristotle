50d63da50076e87426784d90fac3135fdde35930
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-26 09:59:36 -0400

    linked js file to html

--- cs-pilot/script.js
+++ cs-pilot/script.js
@@ -2,8 +2,12 @@ class List {
   constructor(size) {
     var DEFAULT_SIZE = 10;
 
-    // Create a fixed-size array. Start the size at zero.
+    // Create a fixed-size array.
     this.data = new Array(DEFAULT_SIZE);
     this.size = 0;
   }
 }
+
+var myList = new List();
+
+console.log("Total size:", myList.size);
