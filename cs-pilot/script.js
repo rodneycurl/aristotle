bfd3ac26851536b384e407e79479ee1d8ad116b2
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-26 10:33:41 -0400

    adding a new item to my list

--- cs-pilot/script.js
+++ cs-pilot/script.js
@@ -6,8 +6,14 @@ class List {
     this.data = new Array(DEFAULT_SIZE);
     this.size = 0;
   }
+  append(item) {
+  this.data[this.size] = item;
+  this.size++;
+  }
 }
 
 var myList = new List();
+
+myList.append("Yay!");
 
 console.log("Total size:", myList.size);
