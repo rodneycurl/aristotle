4e494ee85ca88006da38044de161ede23da416c3
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-26 10:50:24 -0400

    amending method to check if the list is larger

--- cs-pilot/script.js
+++ cs-pilot/script.js
@@ -7,8 +7,24 @@ class List {
     this.size = 0;
   }
   append(item) {
-  this.data[this.size] = item;
-  this.size++;
+    if (this.size >= this.data.length) {
+      this.growArray();
+      console.log("Fixed size array grew from " + this.size + " to " + this.data.length);
+    }
+
+    this.data[this.size] = item;
+    this.size++;
+  }
+  growArray() {
+    //create a new array twice as big as the current array
+    var newArray = new Array(this.data.length * 2);
+    //copy everything from the old array to the new array
+    for (var i = 0; i < this.data.length; i++) {
+      newArray[i] = this.data[i];
+    }
+
+    //replace the reference from the old array to the new array
+    this.data = newArray;
   }
 }
 
