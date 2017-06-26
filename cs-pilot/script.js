2977e697fd07c8d46916a2fea08337112f2ce399
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-26 08:52:08 -0400

    added a directory for my computer science coursework

---
+++ cs-pilot/script.js
@@ -0,0 +1,12 @@
+class List {
+  constructor(size){
+    var DEFAULT_SIZE = 10;
+
+    // Create a fixed-size array.
+    this.data = new Array(DEFAULT_SIZE);
+  }
+}
+
+var myList = new List();
+
+console.log("Total size:", myList.size);
