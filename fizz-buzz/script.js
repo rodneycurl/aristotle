dd8682945a4cf3f71c9b44b3d68284a46622ff6e
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-30 09:27:22 -0400

    fizz buzz for loop and if statements

--- fizz-buzz/script.js
+++ fizz-buzz/script.js
@@ -1,1 +1,38 @@
-console.log("It's working!")
+// Write a program that prints the integers from 1 to 100 (inclusive).
+  // But:
+  // - for multiples of three, print Fizz (instead of the number)
+  // - for multiples of five, print Buzz (instead of the number)
+  // - for multiples of both three and five, print FizzBuzz (instead of the number)
+
+for (let i = 1; i < 101; i++){
+  if (i % 3 === 0 && i % 5 === 0){
+    console.log("FizzBuzz")
+  }
+  else if (i % 3 === 0) {
+    console.log("Fizz");
+  }
+  else if (i % 5 === 0) {
+    console.log("Buzz");
+  }
+  else {
+    console.log(i);
+  }
+}
+
+
+
+
+
+
+
+
+
+  // Desired outcome:
+  // 1
+  // 2
+  // Fizz
+  // 4
+  // Buzz
+  // ...
+  // FizzBuzz
+  // ...
