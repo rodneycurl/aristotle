27978dd365b11bc8916a3ce509e4ca3c15beffb4
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-06-30 11:36:01 -0400

    adding both a var and let version

--- fizz-buzz/script.js
+++ fizz-buzz/script.js
@@ -4,6 +4,7 @@
   // - for multiples of five, print Buzz (instead of the number)
   // - for multiples of both three and five, print FizzBuzz (instead of the number)
 
+console.log("version 1")
 for (let i = 1; i < 101; i++){
   if (i % 3 === 0 && i % 5 === 0){
     console.log("FizzBuzz")
@@ -19,12 +20,21 @@ for (let i = 1; i < 101; i++){
   }
 }
 
-
-
-
-
-
-
+console.log("version 2")
+for (var i = 1; i < 101; i++){
+  if(i % 3 === 0 && i % 5 === 0){
+    console.log("FizzBuzz");
+  }
+  else if(i % 3 === 0){
+    console.log("Fizz");
+  }
+  else if(i % 5 === 0){
+    console.log("Buzz");
+  }
+  else {
+    console.log(i);
+  }
+}
 
 
   // Desired outcome:
